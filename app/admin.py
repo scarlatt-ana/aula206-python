60a3e7b8bd2fb65a52da63a6c97a832ceb348190
scarlat <scarlattaparecida@gmail.com>
2019-04-30 12:17:17 -0300

    python-admin

---
+++ app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from app.models import Autor, Livro
+
+# Register your models here.
+
+admin.site.register(Autor)
+admin.site.register(Livro)
+#PARA REGISTRAR NO ADMIN APP 
+
---
+++ app/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Create your models here.
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    dt_nascimento = models.DateField()
+    vivo = models.BooleanField(default=True)
+    nacionalidade = models.CharField(max_length=50)
+
+
+class Livro(models.Model):
+    genero_aventura = 'av'
+    genero_religioso = 'rl'
+    genero_auto_ajuda = 'aa'
+    genero_ficcao = 'fc'
+    genero_scifi = 'sf'
+    genero_terror = 'tr'
+    genero_hq = 'hq'
+    genero_manga = 'mg'
+    genero_romance = 'rm'
+    genero_biografia = 'bg'
+    
+    genero_opcoes = [
+        (genero_aventura, 'Aventura'),
+        (genero_religioso, 'Religioso'),
+        (genero_auto_ajuda, 'AutoAjuda'),
+        (genero_ficcao, 'Ficçao'),
+        (genero_scifi, 'Ficçao'),
+        (genero_terror, 'Terror'),
+        (genero_hq, 'Historias em Quadrinhos'),
+        (genero_manga, 'Mangá'),
+        (genero_romance, 'Romance'),
+        (genero_biografia, 'Biografia')
+    ]
+
+
+    titulo = models.CharField(max_length = 100)
+    genero = models.CharField(max_length=2, choices=genero_opcoes)
+    dt_lancamento = models.DateField()
+    descricao = models.TextField(default='')
+    editora = models.CharField(max_length = 50, default='')
+    preco = models.DecimalField(decimal_places=2, max_digits=100, default=0)
+    paginas = models.IntegerField()
+    edicoes = models.IntegerField()
+    nome = models.ForeignKey(Autor, on_delete=models.SET_DEFAULT, default='')
+
+    # CLASSIFICAÇÃO QUE QUER QUE APAREÇA NO SITE                                                                                                                                                                                                                                
